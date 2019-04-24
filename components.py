0fd24d81828f6d3f5eb254c620ff7d6ec61626f8
descamps <adriad@stud.ntnu.no>
2019-04-24 21:37:44 +0200

    Added some functionalities to the node Added classes Consumer and Producer Modified slightly containers PAT and PIT

--- components.py
+++ components.py
@@ -25,59 +25,120 @@ class Packet(object):
         flow_id : int
             small integer that can be used to identify a flow
     """
-    def __init__(self, time, size, name, flow_id=0):
+    def __init__(self, time, size, name, lifetime, antid=None):
         self.time = time
         self.size = size
-        self.name = name
-        #self.flow_id = flow_id
+        self.name = name #The name will contain "/ant/" in case of being a routing packet before the content name
+        self.mode = 0 # 0 means Interest packet, 1 means Data packet
+        self.data = None
+        self.antId = antid
+        self.lifetime = lifetime
 
     def __repr__(self):
         return "name: {}, time: {}, size: {}".\
             format(self.name, self.time, self.size)
 
-class SwitchPort(object):
-    """ Models a switch output port with a given rate and buffer size limit in bytes.
-        Set the "out" member variable to the entity to receive the packet.
+class Consumer(object):
+    def __init__(self):
+        self.antId = random.randrange(9999999)
+        self.interface
+        self.store
 
-        Parameters
-        ----------
-        env : simpy.Environment
-            the simulation environment
-        rate : float
-            the bit rate of the port
-        qlimit : integer (or None)
-            a buffer size limit in bytes or packets for the queue (including items
-            in service).
-        limit_bytes : If true, the queue limit will be based on bytes if false the
-            queue limit will be based on packets.
+    def request_content(self, name):
+        #TODO It will generate packets in a specified interval
+        # It will send 10 ants to form a path and once the first one arrived back in a form of Data packet it will send the Data request
 
-    """
-    def __init__(self, env, rate, qlimit=None, limit_bytes=True, debug=False):
-        self.store = simpy.Store(env)
-        self.rate = rate
-        self.env = env
-        self.out = None
-        self.packets_rec = 0
-        self.packets_drop = 0
-        self.qlimit = qlimit
-        self.limit_bytes = limit_bytes
-        self.byte_size = 0  # Current size of the queue in bytes
-        self.debug = debug
-        self.busy = 0  # Used to track if a packet is currently being sent
-        self.action = env.process(self.run())  # starts the run() method as a SimPy process
+    def listen(self):
+        # TODO It will listen for packets in the store to process
+        # TODO Might use the packet for stadistics and then erase it from memory
+
+
+class Producer(object):
+    def __init__(self, data):
+        self.interface
+        self.store
+        self.data = data #List with the data names of the producer
+
+    def createData(self):
+        # TODO Create some random data
+
+    def listen(self):
+        # TODO It will listen for packets in the store to process
+        # TODO It receive an Interest packet and creates the Data packet for it
+
 
 class Node(object):
-    def __init__(self, name, interfaces):
+    def __init__(self, env, name, interfaces):
+        self.store = simpy.Store(env) #The queue of pkts in the node
         self.name = name
         self.interfaces = interfaces
+        self.timeout = 50 #TODO Assign it properly #It is the time to live in the table
+        self.PAT = PAT()
+        self.PIT = PIT()
+
+    def run(self):
+        while True:
+            item = (yield self.store.get())
+            iface = item[0]
+            pkt = item[1]
+
+            if "/ant/Interest/" in pkt.name and pkt.antId is not None:
+                #Here ant packets process
+                self.antId+=1
+                entry = PATobject(pkt.antId, pkt.name, iface, self.timeout)
+                self.PAT.table[self.antId] = entry #Add the Interest packet
+                out_iface = self.forward_engine(pkt.mode) #The ForwardEngine decides outgoing interface
+                out_iface.out_iface.put(pkt) #The packet is sent to the out iface
+            elif "/Interest/" in pkt.name and pkt.antId is None:
+                #Here content packets are processed
+                if pkt.name in self.PIT.table:
+                    self.PIT.table[pkt.name].incoming[iface] = self.timeout
+                else:
+                    self.PIT.table[pkt.name] = PITobject(pkt.name,iface,self.timeout) #Create entry in the PIT table for the Interest packet
+                    out_iface = self.forward_engine(pkt.mode) #The ForwardEngine decides outgoing interface
+                    out_iface.out_iface.put(pkt) #The packet is sent to the out iface
+            elif "/ant/Data/" in pkt.name and pkt.antId is not None:
+                if pkt.antId in self.PAT.table:
+                    # TODO Create entry in FIB OR UPDATE IT
+                    # TODO Update pheromone in the FIB
+
+                    #Remove entry in PAT
+                    entry = self.PAT.table.pop(pkt.antId)
+                    #Take incoming iface from PAT
+                    in_iface = entry.interface
+                    #Send Data packet back to the incoming interface
+                    in_iface.put(pkt)
+
+            elif "/Data/" in pkt.name and pkt.antId is None:
+                    # TODO Create entry in FIB OR UPDATE IT
+                    # TODO Update pheromone in the FIB
+                    # TODO Cache Data if strategy says so
+
+                    # Remove entry in PIT
+                    # Take incoming iface from PIT
+                    # Send Data packet back to the incoming interface
+                    entry = self.PIT.table.pop(pkt.name) #Retrieve and remove the Interest entry for pkt.name
+                    for in_iface,y in entry.incoming.items(): #Loops the interfaces assigned to that name
+                        in_iface.put(pkt) #sends the pkt further to that interfaces
+            else:
+                #Drop packet
+                print("Wrong packet\n")
+
+    def forward_engine(self,mode):
+        #TODO The holistic function deciding which outgoing interface is going to be chosen
+        #TODO Different function for ants and for content
+        iface = random.choices(self.interfaces) #Just for not getting an error in the meantime
+        return iface
+
 
 class Interface(object):
-    def __init__(self, ):
+    def __init__(self, name, iface, store):
+        self.name = name
+        self.out_iface = iface
+        self.out = store #Gonna point to the Node, consumer or producer with iface store
 
-
-class ForwardEngine(object):
-    def __init__(self, ):
-
+    def put(self, pkt):
+        self.out.put([self,pkt])
 
 class FIB(object):
     def __init__(self):
@@ -86,6 +147,10 @@ class FIB(object):
 class PIT(object):
     def __init__(self, ):
         self.table = list() #Dict with name as a key, values incoming interface
+
+class PAT(object):
+    def __init__(self, ):
+        self.table = dict() #Dict with id as a key, values incoming interface and content name
 
 class CS(object):
     def __init__(self, ):
@@ -99,20 +164,16 @@ class FIBobject(object):
 class PITobject(object):
     def __init__(self, name, interface, lifetime):
         self.name = name
-        self.incoming = dict() #dictionary with (interface,lifetime)
+        self.incoming = {interface: lifetime} #dictionary with (interface,lifetime)
 
 class PATobject(object):
     def __init__(self, id, name, interface, lifetime):
         self.id = id #Serial number of the ant
         self.name = name
-        self.incoming = dict() #dictionary with (interface,lifetime)
+        self.interface = interface
+        self.lifetime = lifetime
 
 class CSobject(object):
-    # def __init__(self, name):
-    #     self.name = name
-    #     self.outgoings = dict() #Dictionary with (interface, pheromone)
-
-    def __init__(self, name, interface, pheromone):
+    def __init__(self, name, data):
         self.name = name
-        self.outgoings = dict() #Dictionary with (interface, pheromone)
-        self.outgoings[interface] = pheromone
+        self.data = data
